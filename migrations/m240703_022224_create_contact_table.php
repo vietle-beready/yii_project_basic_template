913ffc92f28a00af7b3179eb13759aceec0d86e6
vnineoneone <76719885+vnineoneone@users.noreply.github.com>
2024-07-04 15:01:27 +0700

    edit migration create_contact_table

--- migrations/m240703_022224_create_contact_table.php
+++ migrations/m240703_022224_create_contact_table.php
@@ -12,13 +12,23 @@ class m240703_022224_create_contact_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%contact}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull(),
             'email' => $this->string()->notNull(),
             'subject' => $this->string()->notNull(),
             'body' => $this->text()->notNull(),
-        ]);
+        ], $tableOptions);
+
+        $this->createIndex(
+            'idx-contact-email',
+            'contact',
+            'email'
+        );
     }
 
     /**
